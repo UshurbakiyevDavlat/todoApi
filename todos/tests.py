d2516b7135cc2873985f35919f1aa03e382aab1e
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-04 15:54:45 +0600

    added DRF views and tests for them

--- todos/tests.py
+++ todos/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from rest_framework import status
 
 from .models import Todo
 
@@ -17,3 +19,20 @@ class TodoModelTest(TestCase):
         self.assertEqual(self.todo.title, "Learn python")
         self.assertEqual(self.todo.body, "A body text here")
         self.assertEqual(str(self.todo), "Learn python")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={
+                "pk": self.todo.id
+            }),
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Learn python")
