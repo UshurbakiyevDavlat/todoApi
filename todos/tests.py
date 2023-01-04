8bac32ff2c124355164c52177e1bb9861fef8eeb
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-04 15:09:29 +0600

    added Todo model and integrated it to admin, added tests

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="Learn python",
+            body="A body text here"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "Learn python")
+        self.assertEqual(self.todo.body, "A body text here")
+        self.assertEqual(str(self.todo), "Learn python")
